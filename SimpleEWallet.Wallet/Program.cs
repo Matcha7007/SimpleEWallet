f6a43df61fdd83c040b86cd3f20a11f1db21bdfa
Matcha Latte <83999587+Matcha7007@users.noreply.github.com>
2025-04-14 14:14:31 +0700

    Update program cs wallet proj

--- SimpleEWallet.Wallet/Program.cs
+++ SimpleEWallet.Wallet/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+using SimpleEWallet.Wallet.Persistence;
+
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WalletDbContext>(option =>
+{
+	string? serverDbConnectionString = builder.Configuration.GetConnectionString("DbWalletConnection");
+	option.UseNpgsql(serverDbConnectionString);
+});
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
